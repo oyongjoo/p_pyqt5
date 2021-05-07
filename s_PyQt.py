7e52be8634e72d21942a4ce412b6221679d1d0da
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-07 23:34:24 +0900

    QtPy5 를 이용하여 Label 보여주기

---
+++ s_PyQt.py
@@ -0,0 +1,10 @@
+# ch03/03_12.py
+import sys
+from PyQt5.QtWidgets import *
+
+app = QApplication(sys.argv)  # QApplication 객체 생성
+
+label = QLabel("Hello") # label 객체 생성
+label.show()  # show Label 객체
+
+app.exec_()  # 이벤트 루프 생성
