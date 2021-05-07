cfb7adb4a3db8177579f532533a90523ccd2f86a
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-07 23:43:49 +0900

    QtPy5 를 이용하여 Button 보여주기

--- s_PyQt.py
+++ s_PyQt.py
@@ -7,4 +7,7 @@ app = QApplication(sys.argv)  # QApplication 객체 생성
 label = QLabel("Hello") # label 객체 생성
 label.show()  # show Label 객체
 
+btn = QPushButton('Hello')
+btn.show()
+
 app.exec_()  # 이벤트 루프 생성
