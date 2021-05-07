e44dff853b695c4ad31231e158c596377515cc39
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-08 00:33:55 +0900

    QtPy5: Window 에 button 위치 이동

--- 03_14.py
+++ 03_14.py
@@ -7,12 +7,13 @@ from PyQt5.QtGui import *
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        rect = [100, 200, 300, 400]
+        rect = [100, 200, 300, 200]
         self.setGeometry(*rect)
         self.setWindowTitle("PyQt")
         self.setWindowIcon(QIcon('stock_01.png'))
 
         btn = QPushButton("버튼1", self)
+        btn.move(10, 10)
 
 
 app = QApplication(sys.argv)
