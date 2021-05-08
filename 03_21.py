27e77b0ce146466e9b5a6930f7e8babb1da7f6ff
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-08 18:28:16 +0900

    QTimer를 이용하여 1초마다 비튼코인 현재 가격으로 얻어오도록 변경.

--- 03_21.py
+++ 03_21.py
@@ -1,6 +1,7 @@
 # 비트 코인 시세 조회 window
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from PyQt5 import uic
 import pykorbit
 
@@ -11,10 +12,15 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.inquiry)
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.inquiry)
 
     # BTC의 현재 가격을 얻어오는 function
     def inquiry(self):
+        cur_time = QTime.currentTime()
+        str_time = cur_time.toString("hh:mm:ss")
+        self.statusBar().showMessage(str_time)
         price = pykorbit.get_current_price("BTC")
         self.lineEdit.setText(str(price))
 
