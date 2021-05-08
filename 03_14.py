5827f47de397ab8f46231b7c74cb90b644ce0d0a
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-08 17:01:50 +0900

    QtPy5: 주석 추가

--- 03_14.py
+++ 03_14.py
@@ -7,6 +7,7 @@ from PyQt5.QtGui import *
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # 위젯 생성 코드
         rect = [100, 200, 300, 200]
         self.setGeometry(*rect)
         self.setWindowTitle("PyQt")
@@ -19,11 +20,13 @@ class MyWindow(QMainWindow):
         btn2 = QPushButton('버튼2', self)
         btn2.move(10, 40)
 
-    def btn_clicked(self):
+    # 이벤트 처리 코드
+    @staticmethod
+    def btn_clicked():
         print("버튼 클릭")
 
-
-app = QApplication(sys.argv)
+# QApplication 객체 생성 및 이벤트 루트 생성 코드
+app = QApplication(sys.argv)  # 객체 생성
 window = MyWindow()
 window.show()
-app.exec_()
+app.exec_()  # 이벤트 루프
