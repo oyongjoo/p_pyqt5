2483579c8a86b2fc16d960cf6929874d326a05c9
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-09 20:11:31 +0900

    외국인소진율 값에서 오른쪽 버튼 후 copy->Copy select복사된 값으로 soup.select 해주면 좀 더 쉽게 값을 얻을수 있음.

--- 04_07-no_id_tag.py
+++ 04_07-no_id_tag.py
@@ -6,6 +6,7 @@ url = "https://finance.naver.com/item/main.nhn?code=000660"
 html = requests.get(url).text
 
 soup = BeautifulSoup(html, "html5lib")
-tags = soup.select(".lwidth tbody .strong td em")
+# tags = soup.select(".lwidth tbody .strong td em")
+tags = soup.select("#tab_con1 > div:nth-child(3) > table > tbody > tr.strong > td > em")
 for tag in tags:
     print(tag.text)
