c419c03baa46650fd81a4ee601ddef5e2195c284
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-09 18:18:36 +0900

    SK하이닉스 per, dividend 구하는 웹스크레핑 구현

--- 04_01.py
+++ 04_01.py
@@ -2,9 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://finance.naver.com/item/main.nhn?code=000660"
+# url ="https://finance.naver.com/sise/sise_market_sum.nhn?page=1"
 html = requests.get(url).text
 
 soup = BeautifulSoup(html, 'html5lib')
-tags = soup.select("#_per")
+# tags = soup.select("#_per")
+tags = soup.select("#_dvr")
+# tags2 = soup.select("#_market_sum")
 tag = tags[0]
 print(tag.text)
+# market_sum = tags2[0]
+# print(market_sum.text.split())
+
+# stock_head = soup.find("thead").find_all("th")
+# data_head = [head.get_text() for head in stock_head]
+#
+# print(data_head)
