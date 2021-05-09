9287b76a14365c691a91f92c08d1bf6aaf849a21
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-09 09:43:19 +0900

    데코레이터 여러 개 지정하기

--- decorator_closure.py
+++ decorator_closure.py
@@ -1,19 +1,18 @@
-def trace(func):         # 호출할 함수를 매개변수로 받음
+def decorator1(func):         # 호출할 함수를 매개변수로 받음
     def wrapper():       # 호출할 함수를 감싸는 함수
-        print(func.__name__, '함수시작')   # __name__으로 함수 이름 출력
-        func()           # 매개변수로 받은 함수를 호출
-        print(func.__name__, '함수끝')    # wrapper 함수 반환
-
+        print('decorator1')
+        func()
     return wrapper
 
-@trace           # @데코레이터
+def decorator2(func):
+    def wrapper():
+        print('decorator2')
+        func()
+    return wrapper
+
+@decorator1
+@decorator2
 def hello():
     print('hello')
 
-@trace           # @데코레이터
-def world():
-    print('world')
-
-
-hello()     # 함수를 그대로 호출
-world()     # 함수를 그대로 호출
+hello()
