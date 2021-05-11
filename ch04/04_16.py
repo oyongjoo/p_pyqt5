b021f20d69981cd1253eccdded7c019e6f3cc0bb
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-11 11:25:17 +0900

    Pandas Dataframe 을 excel 로 저장/읽기 excel file의 date를 index로 지정.

--- ch04/04_16.py
+++ ch04/04_16.py
@@ -1,4 +1,12 @@
 from pandas import DataFrame
+import os
 data = {"open": [737, 750], "high": [755, 780], "low": [700, 710], "close": [750, 770]}
 df = DataFrame(data, index=['2018-0101', '2018-0102'])
+print(df)
+
+import pandas as pd
+df.to_excel("d:\\ohlc-2.xlsx")
+
+df = pd.read_excel("D:\\Python\\p_pyqt5\\ohlc.xlsx", engine='openpyxl')
+df = df.set_index('date')
 print(df)
