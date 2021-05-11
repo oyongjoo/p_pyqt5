e0b820d31dd8877bba4b2761a3c5f09fbce8590f
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-11 14:30:36 +0900

    bids(매수} 호가를 출력

--- ch05/05_08.py
+++ ch05/05_08.py
@@ -6,5 +6,10 @@ orderbook = pybithumb.get_orderbook("BTC")
 asks = orderbook['asks']
 bids = orderbook['bids']
 
-print(bids)
-print(asks)
+# print(bids)
+# print(asks)
+
+for bid in bids:
+    price = bid['price']
+    quantity = bid['quantity']
+    print(f"매수호가: {price}, 매수잔량: {quantity}")
