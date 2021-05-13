9926cabd57aa9ad31d5d1013587b377c560db11a
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-13 17:54:21 +0900

    GUI 화면에 4개의 ticker의 상승장, 하락장 표시

--- ch05/05_19.py
+++ ch05/05_19.py
@@ -2,10 +2,13 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtCore import *
+import pybithumb
 
 form_class = uic.loadUiType('..\\bull.ui')[0]
 
 class MyWindow(QMainWindow, form_class):
+    tickers = ['BTC', 'ETH', 'BCH', 'ETC']
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -14,8 +17,30 @@ class MyWindow(QMainWindow, form_class):
         timer.start(5000)
         timer.timeout.connect(self.timeout)
 
+        self.tableWidget.setRowCount(len(self.tickers))
+
     def timeout(self):
-        print("5초에요!!")
+        bull_list = ['상승장', '하락장']
+        for i, ticker in enumerate(self.tickers):
+
+            item = QTableWidgetItem(ticker)
+            self.tableWidget.setItem(i, 0, item)
+            price, ma5_last, state = self.get_market_infos(ticker)
+            self.tableWidget.setItem(i, 1, QTableWidgetItem(str(price)))
+            self.tableWidget.setItem(i, 2, QTableWidgetItem(str(ma5_last)))
+            self.tableWidget.setItem(i, 3, QTableWidgetItem(state))
+
+    def get_market_infos(self, ticker):
+        df = pybithumb.get_ohlcv(ticker)
+        ma5 = df['close'].rolling(window=5).mean()
+        ma5_last = ma5[-2]
+        price = pybithumb.get_current_price(ticker)
+        if price > ma5_last:
+            state = '상승장'
+        else:
+            state = '하락장'
+
+        return price, ma5_last, state
 
 app = QApplication(sys.argv)
 win = MyWindow()
