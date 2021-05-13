af23ed61a72b00c1283c417c74fb245c7fb8eb8c
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-13 15:38:10 +0900

    5일 이동평균 데이트 구하기, 종가(close)의 5일간격 평균값을 구한다.

--- ch05/ch05/05_13.py
+++ ch05/ch05/05_13.py
@@ -1,5 +1,11 @@
 # ch05/05_14.py
 import pybithumb
+from talib.abstract import _ta_lib as ta
 
 btc = pybithumb.get_ohlcv("BTC")
-print(btc)
+close = btc['close']
+
+ma5 = close.rolling(window=5).mean()
+
+# ma5 = ta.SMA(close, timeperiod=5)
+print(ma5.head(20))
