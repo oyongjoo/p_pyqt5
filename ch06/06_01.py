16a8a937b398bee173492cad2f71595fd07e6ef2
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-15 18:49:39 +0900

    bithumb 지정가/시장가로 매수하기

--- ch06/06_01.py
+++ ch06/06_01.py
@@ -13,3 +13,17 @@ print(order)
 
 # 시장가로 주문하기
 order = bithumb.buy_market_order("BTC", 1)
+
+# ch06/06_08.py
+# 클래스 생성 생략
+order = bithumb.sell_limit_order("BTC", 4000000, 1)
+print(order)
+
+# 이번에는 잔고를 조회해서 보유 중인 비트코인 수량만큼 지정가 매도 주문을 해보겠습니다.
+
+# ch06/06_09.py
+# 클래스 생성 코드 생략
+unit = bithumb.get_balance("BTC")[0]
+print(unit)
+order = bithumb.sell_limit_order("BTC", 4000000, unit)
+print(order)
