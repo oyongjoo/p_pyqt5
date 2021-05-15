937c6b13a78abc0f13e494635a7d87479beefdd1
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-15 20:08:03 +0900

    현재시간이 자정인지 체크

---
+++ ch06/06_14.py
@@ -0,0 +1,16 @@
+import time
+import datetime
+
+now = datetime.datetime.now()
+mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+
+while True:
+    now = datetime.datetime.now()
+
+    if now == mid:
+        print("자정입니다.")
+        now = datetime.datetime.now()
+        mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+
+    print(now, "vs", mid)
+    time.sleep(1)
