737e05da9861cb9115df9e0019b6a299008b487f
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-15 18:45:32 +0900

    bithumb 지정가/시장가로 매수하기

--- ch06/06_01.py
+++ ch06/06_01.py
@@ -7,6 +7,9 @@ sec_key = "ecc4a67427a408e99d58f5fc3946d481"
 
 bithumb = pybithumb.Bithumb(con_key, sec_key)
 
-# 현재로선 돈이 없어서 못산다.
+# 지정가로 주문하기, 현재로선 돈이 없어서 못산다.
 order = bithumb.buy_limit_order("BTC", 57000000, 0.001)
 print(order)
+
+# 시장가로 주문하기
+order = bithumb.buy_market_order("BTC", 1)
