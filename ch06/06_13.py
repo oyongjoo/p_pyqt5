feba143e8acad081fcee268095f4df2655243057
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-15 19:01:23 +0900

    래리 윌리엄스 변동성 돌파 전략의 목표가 계산하기를 함수로 변경.

--- ch06/06_13.py
+++ ch06/06_13.py
@@ -1,12 +1,15 @@
 import pybithumb
 
-df = pybithumb.get_ohlcv("BTC")
-print(df.tail())
-yesterday = df.iloc[-2]
+def get_target_price(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    print(df.tail())
+    yesterday = df.iloc[-2]
 
-today_open = yesterday['close']
-yesterday_high = yesterday['high']
-yesterday_low = yesterday['low']
-target = today_open + (yesterday_high - yesterday_low) * 0.5
-print(target)
+    today_open = yesterday['close']
+    yesterday_high = yesterday['high']
+    yesterday_low = yesterday['low']
+    target = today_open + (yesterday_high - yesterday_low) * 0.5
+    return target
+
+
 
