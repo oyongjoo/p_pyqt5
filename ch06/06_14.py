30df47572007e56af9a7e249f71972b071df00ac
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-15 20:11:12 +0900

    현재시간이 자정(+10 초)인지 체크

--- ch06/06_14.py
+++ ch06/06_14.py
@@ -2,12 +2,12 @@ import time
 import datetime
 
 now = datetime.datetime.now()
-mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1) # 다음날 자정
 
 while True:
     now = datetime.datetime.now()
 
-    if now == mid:
+    if mid < now < mid + datetime.timedelta(seconds=15): # 현재시간이 자정 + 10초 사이에 있는 경우는 자정이라고 판단.
         print("자정입니다.")
         now = datetime.datetime.now()
         mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
