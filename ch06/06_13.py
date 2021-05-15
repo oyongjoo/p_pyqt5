1f7e59b5d6aff85850c383fca4f51d4133788b11
Yongjoo Lee <oyongjoo@gmail.com>
2021-05-15 18:58:54 +0900

    래리 윌리엄스 변동성 돌파 전략의 목표가 계산하기

---
+++ ch06/06_13.py
@@ -0,0 +1,12 @@
+import pybithumb
+
+df = pybithumb.get_ohlcv("BTC")
+print(df.tail())
+yesterday = df.iloc[-2]
+
+today_open = yesterday['close']
+yesterday_high = yesterday['high']
+yesterday_low = yesterday['low']
+target = today_open + (yesterday_high - yesterday_low) * 0.5
+print(target)
+
